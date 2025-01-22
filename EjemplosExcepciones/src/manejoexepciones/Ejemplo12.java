e97aa6428eaf691f849681e94b762de2012062ce
carloskes <cscorrea@utpl.edu.ec>
2025-01-22 13:04:58 -0500

    actualizado

---
+++ EjemplosExcepciones/src/manejoexepciones/Ejemplo12.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejoexepciones;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo12 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese cuantas operaciones quiere realizar");
+        int valor = entrada.nextInt();
+        int [] resultados = new int[valor];
+        try {
+         for (int i=0; i < resultados.length; i++  ){
+             System.out.println("Ingrese el valor");
+        resultados[i]= entrada.nextInt();
+        
+         }
+             
+        
+        /*Realizar un proceso repetitivo que permita realizar la división de 
+        números ingresados por teclado; el resultado de cada división debe ir 
+        almacenandose en cada posición del arreglo. Considerar las excepciones
+        posibles*/
+        }catch(Exception e) {
+            
+        }
+    }
+}
